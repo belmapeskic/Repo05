c0e357c3f5b6c3c493cc27fff6d91bc0d6c0b97a
belmapeskic <belma.peskic@gmail.com>
2022-04-01 19:13:48 +0200

    Adding file index.js

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+    // only change code below this line
+    var myFirstName = "Belma";
+    var myLastName = "Peskic";
+    var myHomeTown = "Zenica";
+    // only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main()); 
+module.exports = main;
