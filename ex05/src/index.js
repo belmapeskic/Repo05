b4fecf8ea0fee25d9373d8f0efc8d2cc2898f8af
belmapeskic <belma.peskic@gmail.com>
2022-04-01 23:54:41 +0200

    Adding file index.js

---
+++ ex05/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    // only change code below this line
+    var myString = "This is the first string. This is the second string.";
+    var myString = "This is the first string."+"This is the second string.";
+    // myString is "This is the first string. This is the second string.";
+    // only change code above this line
+   return myString;
+
+}
+console.log(main()); 
+module.exports = main;
