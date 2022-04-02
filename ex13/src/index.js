6a4004212c1bf035fd07bca23e0ffa09784ad053
belmapeskic <belma.peskic@gmail.com>
2022-04-02 05:58:27 +0200

    Adding file index.js

---
+++ ex13/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // only change code below this line
+    var lastName = "Tesla";
+    var lastLetterOfLastName = lastName[lastName.length - 1];     // lastLetterOfLastName is "a"
+// only change code above this line
+    return lastLetterOfLastName;
+
+}
+console.log(main()); 
+module.exports = main;
