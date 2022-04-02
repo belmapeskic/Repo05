fd44088b7a0629371ad6435cad8e7711d18b9bc4
belmapeskic <belma.peskic@gmail.com>
2022-04-02 04:14:53 +0200

    Adding file index.js

---
+++ ex09/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // only change code below this line
+    var lastName = "Doe";
+    var lastNameLength = "Doe".length; // 3
+    // only change code above this line
+    return lastNameLength;
+
+}
+console.log(main()); 
+module.exports = main;
