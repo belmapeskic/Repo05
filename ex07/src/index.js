73e50a6e3f3d88a5b5d069c2e39cad6260ac7438
belmapeskic <belma.peskic@gmail.com>
2022-04-02 03:33:16 +0200

    Adding file index.js

---
+++ ex07/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    // only change code below this line
+    var myName = "Belma";
+    var myString = "My name is "+ myName +" and I am well!";
+    // myString is now "My name is Belma and I am well!"
+    // only change code above this line
+   return myString;
+
+}
+console.log(main()); 
+module.exports = main;
